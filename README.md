73e3477319a6130e8078c56a8e054d91ab415103
sebastian__gabriel <espositosebastian@hotmail.com>
2023-10-01 21:04:13 -0300

    Upload first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+//Bienvenido a FreeBook la mejor red social!
+
+Instrucciones de uso: 
+
+
+Front-end:  
+
+Abrir terminal => cd client //
+
+//npm install
+
+//npm run dev
+
+Ejecutar en el puerto iniciado. Si llegara a surgir algun error al cargar, limpiar cache de local-storage e inicar en http://localhost:3000/login/ 
+
+
+
+Back-end: 
+
+Instalar docker y docker compose.
+
+Abrir nueva terminal => cd backend //
+
+Ejecutar docker-compose up -d (Esto iniciará los contenedores del backend en segundo plano (-d) )
+
+//Ejecutar contenedor :  docker-compose up
+
+
+
+//Documentación de end-points : http://localhost:8000/swagger/
+
+//Sobre mí - About me : http://localhost:3000/about
---
+++ client/src/app/about/page.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Navbar } from '@/components/Navbar/Navbar'
+import { useRedux } from '../../hooks/useRedux';
+
+const AboutPage: React.FC = () => {
+    return (        
+        <div className=' bg-dark-100 h-screen '>
+          <section id="about" className="about ">
+            <div className="img-about scroll-scale">
+              <img src="./1681917768006-removebg-preview.png" alt="" />
+              <div className="info-about1">
+                <span>2+</span>
+                <p>Años de experiencia</p>
+              </div>
+              <div className="info-about2">
+                <span>20+</span>
+                <p>Proyectos completos</p>
+              </div>
+              <div className="info-about3">
+                <span>10+</span>
+                <p>Clientes satisfechos</p>
+              </div>
+            </div>
+  
+            <div className="about-content scroll-scale">
+              <span>Permíteme presentarme</span>
+              <h2>Acerca de mí</h2>
+              <h3>Conoce mi historia</h3>
+              <p>
+                Como desarrollador Full Stack con experiencia en múltiples
+                tecnologías, mi perfil profesional está orientado a crear
+                aplicaciones web escalables, atractivas y funcionales tanto desde
+                el Frontend como en el Backend. Soy capaz de trabajar en cada etapa
+                del ciclo de vida de una aplicación, desde la planificación y el
+                diseño hasta la implementación y la optimización.
+              </p>
+            </div>
+          </section>
+        </div>
+    );
+  };
+  
+  export default AboutPage;
